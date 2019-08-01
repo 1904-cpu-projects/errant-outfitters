39ccf69e71d480f13a34ce8f9155eee88a151055
Thom Smith <thom.smith2@gmail.com>
2019-07-31 18:01:26 -0700

    initial commit, its going to work this time

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./src/App";
+import App from "./App";
 //Got to see that HR!
 ReactDOM.render(<App />, document.getElementById("root"));
