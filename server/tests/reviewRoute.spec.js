4e4ae4a92c693dbd3a0f7a1daa9a659453e0f76c
Thom Smith <thom.smith2@gmail.com>
2019-08-05 21:24:27 -0700

    wrote out some thunks

---
+++ server/tests/reviewRoute.spec.js
@@ -0,0 +1,63 @@
+const request = require("supertest");
+const app = require("../server");
+const { Review, db } = require("../db/index");
+
+beforeAll(() => {
+  db.sync();
+});
+
+describe("GET and POST review routes", () => {
+  it("gets all the current reviews", async done => {
+    const response = await request(app).get("/api/reviews");
+    expect(response.status).toEqual(200);
+    expect(response.body.length).toBeGreaterThan(1);
+    done();
+  });
+
+  it("can post a new review", async done => {
+    const response = await request(app)
+      .post("/api/reviews")
+      .send({
+        title: "Cras non efficitur ipsum",
+        author: "Betty",
+        body:
+          "Donec magna ex, iaculis ut augue sed, aliquet aliquam tellus. Mauris dolor lectus, venenatis non ultrices ut, cursus non diam. "
+      });
+    expect(response.status).toEqual(201);
+    expect(response.body.title).toBe("Cras non efficitur ipsum");
+    done();
+  });
+});
+
+describe("Delete and PUT review routes", () => {
+  let bettyReview;
+
+  beforeAll(async () => {
+    bettyReview = await Review.findOne({
+      where: {
+        author: "Betty"
+      }
+    });
+  });
+
+  it("changes the title of Betty's article", async done => {
+    const response = await request(app)
+      .put(`/api/reviews/${bettyReview.id}`)
+      .send({ title: "Where's Preston?" });
+    expect(response.status).toEqual(201);
+    expect(response.body.title).toEqual("Where's Preston?");
+    done();
+  });
+
+  it("Removes the article based on the id", async done => {
+    const removedArticle = await request(app).delete(
+      `/api/reviews/${bettyReview.id}`
+    );
+    const noArticle = await request(app).get(`/api/reviews/${bettyReview.id}`);
+    expect(removedArticle.status).toEqual(404);
+    expect(noArticle.body.name).toEqual(undefined);
+    done();
+  });
+});
+
+afterAll(() => db.sync());
