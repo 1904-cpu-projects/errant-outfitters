b23adae43416f296699fee6a9a30c89781e45101
Thom Smith <thom.smith2@gmail.com>
2019-08-08 08:36:03 -0700

    reviews are linked to products, seed file expanded to include product/review associations

--- src/components/DetailedProduct.js
+++ src/components/DetailedProduct.js
@@ -4,34 +4,38 @@ import { connect } from "react-redux";
 import store from "../store";
 import { getDetailProduct } from "../storeReducers/productsReducer";
 
+import Reviews from "./Reviews";
+
 // More to come for this thing, need reviews, and add to cart
 // Basics are here!!!
 function DetailProduct({ detailProduct, id }) {
-//  console.log(detailProduct);
-  console.log(store.getState());
-  if(detailProduct.id !== id)
-    getDetailProduct(id);
-  if(!detailProduct) return null;
+  //  console.log(detailProduct);
+  if (detailProduct.id !== id) getDetailProduct(id);
+  if (!detailProduct) return null;
   else {
-    return(
+    return (
       <div>
-	<img src={detailProduct.image} className={'product-image'} alt="Product Image"/>
-	<h1>{detailProduct.name}</h1>
-	<div>{detailProduct.description}</div>
-	<div>INSTOCK | {detailProduct.instock ? "YES" : "NO"}</div>
+        <img
+          src={detailProduct.image}
+          className={"product-image"}
+          alt="Product Image"
+        />
+        <h1>{detailProduct.name}</h1>
+        <div>{detailProduct.description}</div>
+        <div>INSTOCK | {detailProduct.instock ? "YES" : "NO"}</div>
+        <footer>
+          <Reviews product={detailProduct} productId={id} />
+        </footer>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(ownProps);
-  return (
-    {
-      detailProduct: state.products.detailProduct,
-      id: ownProps.match.params.id
-    }
-  );
+  return {
+    detailProduct: state.products.detailProduct,
+    id: ownProps.match.params.id
+  };
 };
 
 export default connect(mapStateToProps)(DetailProduct);
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -9,15 +9,21 @@ class Reviews extends React.Component {
   }
   render() {
     const { reviews } = this.props.reviews;
+    const filteredReviews = reviews.filter(
+      item => item.productId === this.props.productId
+    );
+    //reviews need to be filtered based on the product id
+    console.log("REVIEWS", this.props);
+    console.log("FILTERED reviews", filteredReviews);
     return (
       <div>
         <footer>
           <h4>Reviews for the {this.props.product.name}</h4>
           <div>
-            {reviews.map(i => (
-              <div key={i.id}>
-                {i.title}
-                {i.body}
+            {filteredReviews.map(i => (
+              <div className="reviewDiv" key={i.id}>
+                <h4>{i.title}</h4>
+                <h5>{i.body}</h5>
               </div>
             ))}
           </div>
