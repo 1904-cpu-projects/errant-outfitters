bbada285085cd2d617508d2eba068e22f24833ce
Schuyler Lawrence <schuyler.lawrence@gmail.com>
2019-08-11 19:50:36 -0700

    sort btns

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function MenuBar({ products }) {
+export function MenuBar({ products }) {
+  let sortChoice = [];
+  function onHandleAll(event) {
+    sortChoice = products.productList;
+    console.log(sortChoice);
+  }
+  function onHandleArmor(event) {
+    sortChoice = products.productList.filter(a => a.catagory === 'armor');
+    console.log(sortChoice);
+  }
+  function onHandleWeapon(event) {
+    sortChoice = products.productList.filter(a => a.catagory === 'weapon');
+    console.log(sortChoice);
+  }
+  function onHandlePotion(event) {
+    sortChoice = products.productList.filter(a => a.catagory === 'potion');
+    console.log(sortChoice);
+  }
   return (
     <div className="menu-bar">
-      Add menu button here. Such as filtering items. Later project
-      <button> All Products</button>
+      <form onSubmit={onHandleAll}>
+        <button>All Products</button>
+        <br />
+      </form>
+      <form onSubmit={onHandleArmor}>
+        <button>Armor</button>
+        <br />
+      </form>
+      <form onSubmit={onHandleWeapon}>
+        <button>Weapons</button>
+        <br />
+      </form>
+      <form onSubmit={onHandlePotion}>
+        <button>Potions</button>
+      </form>
     </div>
   );
 }
 
-export default MenuBar;
+const mapStateToProps = state => ({
+  products: state.products,
+});
+
+export default connect(mapStateToProps)(MenuBar);
