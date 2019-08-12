5955a4492bdb81f6e96d016459d17b2a0812c098
Thom Smith <thom.smith2@gmail.com>
2019-08-12 07:08:00 -0700

    starting to thunk products

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -4,3 +4,10 @@ export const FETCH_SINGLE_REVIEW = 'FETCH_SINGLE_REVIEW';
 export const POST_REVIEW = 'POST_REVIEW';
 export const EDIT_REVIEW = 'EDIT_REVIEW';
 export const DELETE_REVIEW = 'DELETE_REVIEW';
+
+//Product Actions
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+export const SET_DETAIL_PRODUCT = 'SET_DETAIL_PRODUCT';
+export const POST_PRODUCT = 'POST_PRODUCT';
+export const EDIT_PRODUCT = 'EDIT_PRODUCT';
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
---
+++ src/actions/productActions.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+import {
+  SET_PRODUCTS,
+  SET_DETAIL_PRODUCT,
+  POST_PRODUCT,
+  EDIT_PRODUCT,
+  DELETE_PRODUCT,
+} from './actionTypes';
+
+export const listProductsThunk = () => async dispatch => {
+  try {
+    const response = await axios.get('/api/products');
+    dispatch({ type: SET_PRODUCTS, payload: response.data });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const singleReviewThunk = id => async dispatch => {
+  try {
+    const response = await axios.get(`/api/products/${id}`);
+    dispatch({ type: SET_DETAIL_PRODUCT, payload: response.data });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const postProductThunk = review => async dispatch => {
+  try {
+    const response = await axios.post('/api/products', review);
+    dispatch({ type: POST_PRODUCT, payload: response.data });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const editProductThunk = (id, change) => async dispatch => {
+  try {
+    const response = await axios.put(`/api/products/${id}`, change);
+    dispatch({ type: EDIT_PRODUCT, payload: response.data });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteProductThunk = product => async dispatch => {
+  try {
+    await axios.delete(`/api/products/${product.id}`);
+    dispatch({ type: DELETE_PRODUCT, payload: product });
+  } catch (err) {
+    console.log(err);
+  }
+};
