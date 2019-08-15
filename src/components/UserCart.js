febc7be8f5527106faadce4b6948cde9e4edb8ff
SamOlivas <sam.a.olivas@gmail.com>
2019-08-15 15:22:15 -0700

    reconcile purchases

--- src/components/UserCart.js
+++ src/components/UserCart.js
@@ -29,7 +29,7 @@ class UserCart extends React.Component {
     const { cart } = this.props;
     const reply = await axios.post('/api/checkout', cart);
     const checkoutId = reply.data;
-    console.log(checkoutId);
+    const reconcileReply = await axios.post('/api/checkout/reconcile', cart);
     await stripe.redirectToCheckout({
       sessionId: checkoutId,
     });
