f7e6dc86e8ef3f309621b41b73644a769e463b00
Thom Smith <thom.smith2@gmail.com>
2019-08-14 20:34:20 -0700

    added menubar import back into branch

--- src/components/DetailedProduct.js
+++ src/components/DetailedProduct.js
@@ -8,6 +8,7 @@ import {
   deleteProductThunk,
   singleProductThunk,
 } from '../actions/productActions';
+import MenuBar from './MenuBar';
 
 function handleBuy(createItem, matchId, quantity) {
   if (quantity) createItem(matchId, quantity);
