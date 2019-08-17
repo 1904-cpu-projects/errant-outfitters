b0da7338f40077bd548ef05d6e6206bcf0c9e717
Nicholas Napoli <nicknapoli82@gmail.com>
2019-08-16 18:36:20 -0700

    Fixed the Cart error mess that I made - Maybe not better code though

--- server/routes/checkout.js
+++ server/routes/checkout.js
@@ -11,31 +11,31 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.post('/reconcile', async (req,res,next) => {
+router.post('/reconcile', async (req, res, next) => {
   const cart = req.body;
-  try{
-    const removeCart = await cart.map( (item) => {
+  try {
+    const removeCart = await cart.map(item => {
       Cart.destroy({
-        where: { id: item.id}
-      })
+        where: { id: item.id },
+      });
     });
-    const reduceStock = await cart.map( (item) => {
-      Product.decrement(
-        'stock',
-        {by: item.quantity, where: {id: item.productId}}
-      )
+    const reduceStock = await cart.map(item => {
+      Product.decrement('stock', {
+        by: item.quantity,
+        where: { id: item.productId },
+      });
     });
-    const postTransaction = await cart.map( (item) => {
+    const postTransaction = await cart.map(item => {
       Transaction.create({
         quantity: item.quantity,
         totalCost: item.quantity * item.product.cost,
         productId: item.productId,
-        userId: item.memberId
-      })
-    })
-    res.send('purchase reconciled')
-  } catch(e) {
+        userId: item.memberId,
+      });
+    });
+    res.send('purchase reconciled');
+  } catch (e) {
     next(e);
   }
-})
+});
 module.exports = router;
