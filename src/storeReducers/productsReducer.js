29d10044fb119a1d2b77e3c4720f287cc36886db
Thom Smith <thom.smith2@gmail.com>
2019-08-18 11:54:47 -0700

    removed old comments

--- src/storeReducers/productsReducer.js
+++ src/storeReducers/productsReducer.js
@@ -6,10 +6,6 @@ import {
   POST_PRODUCT,
 } from '../actions/actionTypes';
 
-// list is the literal list of products that get loaded in from the server api
-// next is the conditional route which gets sent to the server to get the next
-// 25 or so results from a products query
-
 const init = {
   productList: [],
   detailProduct: {},
@@ -18,13 +14,9 @@ const init = {
 
 // And of course the reducer
 export default (products = init, action) => {
-  // Just the boilerplace
-  // Need to add more here soon
   const newProducts = { ...products };
   switch (action.type) {
     case SET_PRODUCTS:
-      // This is the initial load, there should be nothing here
-      // at this point in time
       newProducts.productList = [...action.list];
       break;
     case SET_DETAIL_PRODUCT:
