ba4a00895bd8184b4ba261b29e6f2960fd557de4
Schuyler Lawrence <schuyler.lawrence@gmail.com>
2019-08-20 17:10:06 -0700

    menubarCSS

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -8,60 +8,76 @@ function selector(e) {
 function MenuBar() {
   return (
     <div id="menu-bar">
-      <NavLink className="navlink" exact to="/">
-        <button>All Products</button>
-      </NavLink>
-      <NavLink className="navlink" exact to="/armor">
-        <button>Armor</button>
-      </NavLink>
-      <NavLink className="navlink" exact to="/weapon">
-        <button>Weapons</button>
-      </NavLink>
-      <NavLink className="navlink" exact to="/potion">
-        <button>Potions</button>
-      </NavLink>
-      <NavLink className="navlink" exact to="/ClassProducts">
-        <button>Class Products</button>
-      </NavLink>
+      <ul>
+        <li>
+          <NavLink className="navlink" exact to="/">
+            All Products
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="navlink" exact to="/armor">
+            Armor
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="navlink" exact to="/weapon">
+            Weapons
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="navlink" exact to="/potion">
+            Potions
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="navlink" exact to="/ClassProducts">
+            Class Products
+          </NavLink>
+        </li>
+      </ul>
       <div id="menu-bar">
-        <select onChange={selector}>
-          <option key="0" value="">
-            Sort Options
-          </option>
-          <option key="1" value="/costA">
-            Product Cost Ascending
-          </option>
-          <option key="2" value="/costD">
-            Product Cost Descending
-          </option>
-          <option key="3" value="/stockA">
-            Stock Count Ascending
-          </option>
-          <option key="4" value="/stockD">
-            Stock Count Descending
-          </option>
-          <option key="5" value="/inStock">
-            In Stock
-          </option>
-          <option key="6" value="/outStock">
-            Out of Stock
-          </option>
-          <option key="7" value="/warrior">
-            Product Class: Warrior
-          </option>
-          <option key="8" value="/mage">
-            Product Class: Mage
-          </option>
-          <option key="9" value="/rouge">
-            Product Class: Rouge
-          </option>
-          <option key="10" value="/pnameA">
-            Product Name Ascending
-          </option>
-          <option key="11" value="/pnameD">
-            Product Name Descending
-          </option>
-        </select>
+        <ul className="ul-drop">
+          <li>
+            <select onChange={selector}>
+              <option key="0" value="">
+                Sort Options
+              </option>
+              <option key="1" value="/costA">
+                Product Cost Ascending
+              </option>
+              <option key="2" value="/costD">
+                Product Cost Descending
+              </option>
+              <option key="3" value="/stockA">
+                Stock Count Ascending
+              </option>
+              <option key="4" value="/stockD">
+                Stock Count Descending
+              </option>
+              <option key="5" value="/inStock">
+                In Stock
+              </option>
+              <option key="6" value="/outStock">
+                Out of Stock
+              </option>
+              <option key="7" value="/warrior">
+                Product Class: Warrior
+              </option>
+              <option key="8" value="/mage">
+                Product Class: Mage
+              </option>
+              <option key="9" value="/rouge">
+                Product Class: Rouge
+              </option>
+              <option key="10" value="/pnameA">
+                Product Name Ascending
+              </option>
+              <option key="11" value="/pnameD">
+                Product Name Descending
+              </option>
+            </select>
+          </li>
+        </ul>
       </div>
     </div>
   );
